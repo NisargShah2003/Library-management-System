95c06d09d6d6424c5e186b4e60fcf890d6c1a12d
Nisarg Shah <104247171+NisargShah2003@users.noreply.github.com>
2023-03-17 16:55:42 +0530

    Update db.py

--- db.py
+++ db.py
@@ -11,9 +11,11 @@ def getAvailableBooks():
         
         conn.commit()
         conn.close()
+        return [True]
 
     except Exception as e:
         print(e)
+        return [False,e]
         
 def AddBook(name,price):
     try:
@@ -33,9 +35,11 @@ def AddBook(name,price):
 
         conn.commit()
         conn.close()
+        return [True]
 
     except Exception as e:
         print(e)
+        return [False,e]
         
 def AddUser(name,Pass,email):
     try:
@@ -53,9 +57,12 @@ def AddUser(name,Pass,email):
 
         conn.commit()
         conn.close()
+        return [True]
         
     except Exception as e:
         print(e)
+        
+        return [False,e]
         
 def RemoveUser(name):
     try:
@@ -67,9 +74,11 @@ def RemoveUser(name):
 
         conn.commit()
         conn.close()
-        
+        return [True]
+
     except Exception as e:
         print(e)
+        return [False,e]
 
 def BorrowBook(uname,bname):
     try:
@@ -89,18 +98,18 @@ def BorrowBook(uname,bname):
             val = num[0]
             cursor.execute("""insert into borrow_books(user_name,book_name) values (?,?)""",(uname,bname,)) #add into borrowed list
             cursor.execute("update books set isBorrow=1 where rowid = (?)",(val,)) #Mark borrowed true for books in library
-            print("The book has been added successfully!")
+            conn.commit()
+            conn.close()
+            return [True]
         else:
-            print("The requested book is not available")
-            print()
-            print("Here are the available books:")
-            getAvailableBooks()
-
+            conn.commit()
+            conn.close()
+            return [False,"There is no book available which you want"]
+            
+    except Exception as e:
         conn.commit()
         conn.close()
-
-    except Exception as e:
-        print(e)
+        return [False,e]
 
 
 def getBorrowedBooks(name):
@@ -113,9 +122,12 @@ def getBorrowedBooks(name):
         print(cursor.fetchall())
         conn.commit()
         conn.close()
+        return [True]
 
     except Exception as e:
-        print(e)
+        conn.commit()
+        conn.close()
+        return [False,e]
 
 def checkUser(name,Pass):
     try:
@@ -123,19 +135,23 @@ def checkUser(name,Pass):
         cursor = conn.cursor()
         
         cursor.execute("select Pass from users where name = (?)",(name,))
-        val = cursor.fetchall()[0][0]
-        print(val)
+        val = cursor.fetchall()
+        if val == []:
+            return [False,"Username does not exist!"]
+        else:
+            val = val[0][0]
 
         conn.commit()
         conn.close()
         
         if val == (Pass):
-            return True
+            return [True]
         else:
-            return False
+            return [False,"Password does not match!"]
 
     except Exception as e:
         print(e)
+        return [False,e]
         
 def ReturnBook(uname,bname):
     try:
@@ -146,25 +162,23 @@ def ReturnBook(uname,bname):
         cursor.execute("select rowid from borrow_books where book_name=(?) and user_name=(?)",(bname,uname,))
         temp = cursor.fetchall()
         num = temp[0]
-        print(num)
         
         if num != []:
             #Mark borrowed false for books in library
             cursor.execute("select book_name from borrow_books where rowid=(?)",(num)) 
             temp = cursor.fetchall()
-            print("temp:",temp)
+            
             num1 = temp[0]
             cursor.execute("update books set isBorrow=0 where name = (?)",(num1)) 
             cursor.execute("delete from borrow_books where rowid =(?)",(num)) #Mark borrowed false for books in library
             
+            conn.commit()
+            conn.close()
+            return [True]
         else:
-            print("you cannot return the book you have not borrowed!")
-            print()
-            print("Here is the list of borrowed books:")
-            getBorrowedBooks(uname)
-
-        conn.commit()
-        conn.close()
-
+            conn.commit()
+            conn.close()
+            return [False,"you cannot return the book you have not borrowed!"]
+    
     except Exception as e:
-        print(e)
+        return [False,e]
